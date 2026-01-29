e03579893428a335bf0e62d54e794ebc44777068
Jim Bridger <retrotheft@gmail.com>
2026-01-30 06:44:21 +1100

    added EmbeddedGame

--- sites/jimbridger-dev/src/lib/EmbeddedGame.svelte
+++ sites/jimbridger-dev/src/lib/EmbeddedGame.svelte
@@ -1,3 +1,7 @@
-<script lang='ts'>
-   
-</script>
+<svelte:head>
+  <script src="https://cdn.jsdelivr.net/gh/retrotheft/jimbridger-monorepo@main/cdn/games/tetris.iife.js" defer></script>
+  <link rel="stylesheet" href="/games/tetris/tetris.css" />
+  <link rel="stylesheet" href="/games/tetris/style.css" />
+</svelte:head>
+
+<div id="app"></div>
--- sites/jimbridger-dev/src/routes/+page.svelte
+++ sites/jimbridger-dev/src/routes/+page.svelte
@@ -2,8 +2,11 @@
    import Minesweeper from '$lib/games/Minesweeper.svelte'
    import Snake from '$lib/games/Snake.svelte'
    import Tetris from '$lib/games/Tetris.svelte'
+
+   import EmbeddedGame from '$lib/EmbeddedGame.svelte'
 </script>
 
-<Minesweeper />
+<!-- <Minesweeper /> -->
 <!-- <Snake /> -->
 <!-- <Tetris /> -->
+<EmbeddedGame />
