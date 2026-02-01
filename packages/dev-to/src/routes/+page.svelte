1e7f010a9639b68d63d0fab1e6c749c9467400ac
Jim Bridger <retrotheft@gmail.com>
2026-02-02 07:11:08 +1100

    added dev-to library

---
+++ packages/dev-to/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+   import { getArticles } from "$lib/articles.remote.js";
+   import ArticleCompact from '$lib/components/ArticleCompact.svelte'
+
+   const articles = getArticles()
+
+   $effect(() => {
+      console.log(articles.current)
+   })
+</script>
+
+{#each await getArticles() as article}
+   <ArticleCompact {article} />
+{/each}
